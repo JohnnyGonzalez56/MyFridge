ba7671be689c0ee4a19bed6cfd3a42834f2742df
John Gonzalez <gonjohn29@gmail.com>
2025-12-18 23:07:43 -0700

    bootstrap front and back ends

---
+++ fridge-api/fridge-api/src/main/java/com/johngonzalez/fridge_api/FridgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.johngonzalez.fridge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeApiApplication.class, args);
+	}
+
+}
---
+++ fridge-api/fridge-api/src/test/java/com/johngonzalez/fridge_api/FridgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johngonzalez.fridge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
